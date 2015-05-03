419a53d98180d77629d792244790d316f8b00176
zaneli <shun.otani@gmail.com>
2015-05-03 21:54:27 +0900

    add Value class

--- src/main/scala/com/zaneli/kyototycoon4s/KyotoTycoonClient.scala
+++ src/main/scala/com/zaneli/kyototycoon4s/KyotoTycoonClient.scala
@@ -12,7 +12,6 @@ object KyotoTycoonClient {
 class RestClient(private[this] val host: String, private[this] val port: Int) {
 
   private[this] val baseUrl = s"http://$host:$port"
-  private[this] val charset = "UTF-8"
 
   def get(key: String): Either[Response, Response] = {
     call(key, "get")
@@ -22,15 +21,15 @@ class RestClient(private[this] val host: String, private[this] val port: Int) {
     call(key, "head")
   }
 
-  def set(key: String, value: Array[Byte], xt: Option[Int] = None): Either[Response, Response] = {
+  def set(key: String, value: Value, xt: Option[Int] = None): Either[Response, Response] = {
     put(key, value, "set", xt)
   }
 
-  def add(key: String, value: Array[Byte], xt: Option[Int] = None): Either[Response, Response] = {
+  def add(key: String, value: Value, xt: Option[Int] = None): Either[Response, Response] = {
     put(key, value, "add", xt)
   }
 
-  def replace(key: String, value: Array[Byte], xt: Option[Int] = None): Either[Response, Response] = {
+  def replace(key: String, value: Value, xt: Option[Int] = None): Either[Response, Response] = {
     put(key, value, "replace", xt)
   }
 
@@ -38,27 +37,26 @@ class RestClient(private[this] val host: String, private[this] val port: Int) {
     call(key, "delete")
   }
 
-  private[this] def put(key: String, value: Array[Byte], mode: String, xt: Option[Int]): Either[Response, Response] = {
+  private[this] def put(key: String, value: Value, mode: String, xt: Option[Int]): Either[Response, Response] = {
     val headers = ("X-Kt-Mode", mode) +: xt.map(x => ("X-Kt-Xt", x.toString)).toSeq
     call(key, "put", value, headers)
   }
 
   private[this] def url(key: String): String = {
-    val path = URLEncoder.encode(key, charset)
+    val path = URLEncoder.encode(key, "UTF-8")
     s"$baseUrl/$path"
   }
 
   private[this] def call(
       key: String,
       method: String,
-      body: Array[Byte] = Array.empty[Byte],
+      body: Value = Value.empty,
       headers: Seq[(String, String)] = Seq.empty[(String, String)]): Either[Response, Response] = {
-    val req = Http(url(key)).method(method).timeout(connTimeoutMs = 5000, readTimeoutMs = 5000).headers(headers)
-    val res = (if (body.nonEmpty) {
-      req.postData(body)
+    val res = (if (body.content.nonEmpty) {
+      Http(url(key)).postData(body.content)
     } else {
-      req
-    }).asString
+      Http(url(key))
+    }).method(method).headers(headers).timeout(connTimeoutMs = 5000, readTimeoutMs = 5000).asString
     if (res.isError) {
       Left(Response(res.code, res.body, res.headers))
     } else {
@@ -68,3 +66,14 @@ class RestClient(private[this] val host: String, private[this] val port: Int) {
 }
 
 case class Response(code: Int, body: String, headers: Map[String, String])
+
+case class Value(content: Array[Byte])
+
+object Value {
+  def apply(content: String): Value = {
+    Value(content.getBytes("UTF-8"))
+  }
+  def empty: Value = {
+    Value(Array.empty[Byte])
+  }
+}
