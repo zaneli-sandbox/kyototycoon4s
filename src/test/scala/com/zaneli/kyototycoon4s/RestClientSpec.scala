80bd381954ee99f2d182d4528e8c5403c3c8fda2
zaneli <shun.otani@gmail.com>
2015-05-04 05:14:52 +0900

    add unit test for add

--- src/test/scala/com/zaneli/kyototycoon4s/RestClientSpec.scala
+++ src/test/scala/com/zaneli/kyototycoon4s/RestClientSpec.scala
@@ -3,18 +3,29 @@ package com.zaneli.kyototycoon4s
 import com.github.nscala_time.time.Imports.DateTime
 import java.net.URLEncoder
 import java.util.Arrays
-import org.scalatest.FunSpec
+import org.scalatest.{BeforeAndAfter, FunSpec}
+import scala.collection.mutable.ListBuffer
+import scala.util.Try
 import scalaj.http.Http
 
-class RestClientSpec extends FunSpec {
+class RestClientSpec extends FunSpec with BeforeAndAfter {
 
   private[this] val (host, port) = ("localhost", 1978)
 
   private[this] val client = KyotoTycoonClient.rest(host, port)
 
+  private[this] val keys: ListBuffer[String] = ListBuffer()
+
+  after {
+    keys.foreach { key =>
+      Try(Http(url(URLEncoder.encode(key, "UTF-8"))).method("delete").asString)
+    }
+    keys.clear()
+  }
+
   describe("getString") {
     it("value exists") {
-      val key = "test_key"
+      val key = asKey("test_key")
       val value = "test_value"
       prepare(key, value)
       val res = client.getString(key)
@@ -25,7 +36,7 @@ class RestClientSpec extends FunSpec {
       }
     }
     it("value with xt exists") {
-      val key = "test_key_xt"
+      val key = asKey("test_key_xt")
       val value = "test_value_xt"
       val xt = DateTime.now.plusMinutes(10)
       prepare(key, value, Some(xt.getMillis / 1000))
@@ -37,7 +48,7 @@ class RestClientSpec extends FunSpec {
       }
     }
     it("value exists (key require url encode)") {
-      val key = "te st/key?=%~"
+      val key = asKey("te st/key?=%~")
       val value = "te st/value?=%~"
       prepare(URLEncoder.encode(key, "UTF-8"), value)
       val res = client.getString(key)
@@ -56,7 +67,7 @@ class RestClientSpec extends FunSpec {
 
   describe("getBytes") {
     it("value exists") {
-      val key = "test_key"
+      val key = asKey("test_key")
       val value = "test_value"
       prepare(key, value)
       val res = client.getBytes(key)
@@ -67,7 +78,7 @@ class RestClientSpec extends FunSpec {
       }
     }
     it("value with xt exists") {
-      val key = "test_key_xt"
+      val key = asKey("test_key_xt")
       val value = "test_value_xt"
       val xt = DateTime.now.plusMinutes(10)
       prepare(key, value, Some(xt.getMillis / 1000))
@@ -79,7 +90,7 @@ class RestClientSpec extends FunSpec {
       }
     }
     it("value exists (key require url encode)") {
-      val key = "te st/key?=%~"
+      val key = asKey("te st/key?=%~")
       val value = "te st/value?=%~"
       prepare(URLEncoder.encode(key, "UTF-8"), value)
       val res = client.getBytes(key)
@@ -98,7 +109,7 @@ class RestClientSpec extends FunSpec {
 
   describe("head") {
     it("value exists") {
-      val key = "test_key"
+      val key = asKey("test_key")
       val value = "test_value"
       prepare(key, value)
       val res = client.head(key)
@@ -109,7 +120,7 @@ class RestClientSpec extends FunSpec {
       }
     }
     it("value with xt exists") {
-      val key = "test_key_xt"
+      val key = asKey("test_key_xt")
       val value = "test_value_xt"
       val xt = DateTime.now.plusMinutes(10)
       prepare(key, value, Some(xt.getMillis / 1000))
@@ -121,7 +132,7 @@ class RestClientSpec extends FunSpec {
       }
     }
     it("value exists (key require url encode)") {
-      val key = "te st/key?=%~"
+      val key = asKey("te st/key?=%~")
       val value = "te st/value?=%~"
       prepare(URLEncoder.encode(key, "UTF-8"), value)
       val res = client.head(key)
@@ -140,7 +151,7 @@ class RestClientSpec extends FunSpec {
 
   describe("set") {
     it("set string value") {
-      val key = "test_key_for_set_string"
+      val key = asKey("test_key_for_set_string")
       val value = "test_value_for_set_string"
       assert(client.set(key, Value(value)).isSuccess)
 
@@ -150,7 +161,7 @@ class RestClientSpec extends FunSpec {
       assert(getXt(res.headers).isEmpty)
     }
     it("set bytes value") {
-      val key = "test_key_for_set_bytes"
+      val key = asKey("test_key_for_set_bytes")
       val value = "test_value_for_set_bytes".getBytes("UTF-8")
       assert(client.set(key, Value(value)).isSuccess)
 
@@ -160,7 +171,7 @@ class RestClientSpec extends FunSpec {
       assert(getXt(res.headers).isEmpty)
     }
     it("set value with xt") {
-      val key = "test_key_for_set_with_xt"
+      val key = asKey("test_key_for_set_with_xt")
       val value = "test_value_for_set_with_xt"
       val xt = DateTime.now.plusMinutes(10)
       assert(client.set(key, Value(value), Some(xt)).isSuccess)
@@ -171,7 +182,7 @@ class RestClientSpec extends FunSpec {
       assert(getXt(res.headers).contains(xt.withMillisOfSecond(0)))
     }
     it("set value (key require url encode)") {
-      val key = "te st/key_for_set?=%~"
+      val key = asKey("te st/key_for_set?=%~")
       val value = "te st/value_for_set?=%~"
       assert(client.set(key, Value(value)).isSuccess)
 
@@ -182,9 +193,67 @@ class RestClientSpec extends FunSpec {
     }
   }
 
+  describe("add") {
+    it("add string value") {
+      val key = asKey("test_key_for_add_string")
+      val value = "test_value_for_add_string"
+      assert(client.add(key, Value(value)).isSuccess)
+
+      val res = Http(url(key)).asString
+      assert(res.isNotError)
+      assert(res.body === value)
+      assert(getXt(res.headers).isEmpty)
+    }
+    it("add bytes value") {
+      val key = asKey("test_key_for_add_bytes")
+      val value = "test_value_for_add_bytes".getBytes("UTF-8")
+      assert(client.add(key, Value(value)).isSuccess)
+
+      val res = Http(url(key)).asBytes
+      assert(res.isNotError)
+      assert(Arrays.equals(res.body, value))
+      assert(getXt(res.headers).isEmpty)
+    }
+    it("add value with xt") {
+      val key = asKey("test_key_for_add_with_xt")
+      val value = "test_value_for_add_with_xt"
+      val xt = DateTime.now.plusMinutes(10)
+      assert(client.add(key, Value(value), Some(xt)).isSuccess)
+
+      val res = Http(url(key)).asString
+      assert(res.isNotError)
+      assert(res.body === value)
+      assert(getXt(res.headers).contains(xt.withMillisOfSecond(0)))
+    }
+    it("add value (key require url encode)") {
+      val key = asKey("te st/key_for_add?=%~")
+      val value = "te st/value_for_add?=%~"
+      assert(client.add(key, Value(value)).isSuccess)
+
+      val res = Http(url(URLEncoder.encode(key, "UTF-8"))).asString
+      assert(res.isNotError)
+      assert(res.body === value)
+      assert(getXt(res.headers).isEmpty)
+    }
+    it("add value (already key exists)") {
+      val key = asKey("test_key_for_add_string")
+      val value = "test_value_for_add_string"
+      assert(client.set(key, Value(value)).isSuccess)
+
+      val res = client.add(key, Value(value))
+      assert(res.isFailure)
+      assert(res.failed.get.getMessage === "450: DB: 6: record duplication: record duplication")
+    }
+  }
+
   private[this] def prepare(key: String, value: String, xt: Option[Long] = None): Unit = {
     val req = Http(url(key)).postData(value).method("put")
     xt.fold(req)(x => req.header("X-Kt-Xt", x.toString)).asString
+  }
+
+  private[this] def asKey(key: String): String = {
+    keys.append(key)
+    key
   }
 
   private[this] def url(key: String): String = {
