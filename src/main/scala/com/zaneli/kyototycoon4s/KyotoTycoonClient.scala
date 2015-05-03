ed1a78271bbaac0fea0a3c5ff360f0e25c835fbb
zaneli <shun.otani@gmail.com>
2015-05-03 23:13:43 +0900

    modify error handling

--- src/main/scala/com/zaneli/kyototycoon4s/KyotoTycoonClient.scala
+++ src/main/scala/com/zaneli/kyototycoon4s/KyotoTycoonClient.scala
@@ -1,6 +1,7 @@
 package com.zaneli.kyototycoon4s
 
 import java.net.URLEncoder
+import scala.util.{Failure, Success, Try}
 import scalaj.http.Http
 
 object KyotoTycoonClient {
@@ -13,31 +14,31 @@ class RestClient(private[this] val host: String, private[this] val port: Int) {
 
   private[this] val baseUrl = s"http://$host:$port"
 
-  def get(key: String): Either[Response, Response] = {
+  def get(key: String): Try[Response] = {
     call(key, "get")
   }
 
-  def head(key: String): Either[Response, Response] = {
+  def head(key: String): Try[Response] = {
     call(key, "head")
   }
 
-  def set(key: String, value: Value, xt: Option[Int] = None): Either[Response, Response] = {
+  def set(key: String, value: Value, xt: Option[Int] = None): Try[Response] = {
     put(key, value, "set", xt)
   }
 
-  def add(key: String, value: Value, xt: Option[Int] = None): Either[Response, Response] = {
+  def add(key: String, value: Value, xt: Option[Int] = None): Try[Response] = {
     put(key, value, "add", xt)
   }
 
-  def replace(key: String, value: Value, xt: Option[Int] = None): Either[Response, Response] = {
+  def replace(key: String, value: Value, xt: Option[Int] = None): Try[Response] = {
     put(key, value, "replace", xt)
   }
 
-  def delete(key: String): Either[Response, Response] = {
+  def delete(key: String): Try[Response] = {
     call(key, "delete")
   }
 
-  private[this] def put(key: String, value: Value, mode: String, xt: Option[Int]): Either[Response, Response] = {
+  private[this] def put(key: String, value: Value, mode: String, xt: Option[Int]): Try[Response] = {
     val headers = ("X-Kt-Mode", mode) +: xt.map(x => ("X-Kt-Xt", x.toString)).toSeq
     call(key, "put", value, headers)
   }
@@ -51,21 +52,22 @@ class RestClient(private[this] val host: String, private[this] val port: Int) {
       key: String,
       method: String,
       body: Value = Value.empty,
-      headers: Seq[(String, String)] = Seq.empty[(String, String)]): Either[Response, Response] = {
-    val res = (if (body.content.nonEmpty) {
+      headers: Seq[(String, String)] = Seq.empty[(String, String)]): Try[Response] = {
+    val req = (if (body.content.nonEmpty) {
       Http(url(key)).postData(body.content)
     } else {
       Http(url(key))
-    }).method(method).headers(headers).timeout(connTimeoutMs = 5000, readTimeoutMs = 5000).asString
-    if (res.isError) {
-      Left(Response(res.code, res.body, res.headers))
-    } else {
-      Right(Response(res.code, res.body, res.headers))
+    }).method(method).headers(headers).timeout(connTimeoutMs = 5000, readTimeoutMs = 5000)
+    Try(req.asString).flatMap {
+      case res if res.isError => Failure(new KyotoTycoonException(res.code, res.headers.get("X-Kt-Error")))
+      case res => Success(Response(res.code, res.body, res.headers))
     }
   }
 }
 
 case class Response(code: Int, body: String, headers: Map[String, String])
+
+class KyotoTycoonException(code: Int, error: Option[String]) extends Exception(s"$code: ${error.getOrElse("")}")
 
 case class Value(content: Array[Byte])
 
