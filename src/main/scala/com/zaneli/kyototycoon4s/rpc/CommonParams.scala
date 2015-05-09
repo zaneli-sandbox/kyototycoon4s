39fae4ff0ad8ac91f04d8832ea34d51085509a14
zaneli <shun.otani@gmail.com>
2015-05-10 00:40:49 +0900

    modify CommonParams

--- src/main/scala/com/zaneli/kyototycoon4s/rpc/CommonParams.scala
+++ src/main/scala/com/zaneli/kyototycoon4s/rpc/CommonParams.scala
@@ -1,14 +1,32 @@
 package com.zaneli.kyototycoon4s.rpc
 
 case class CommonParams(
-    db: Option[String] = None, cur: Option[String] = None, waitKey: Option[String] = None, waitTime: Option[Int] = None) {
+    db: Option[String] = None,
+    cur: Option[Int] = None,
+    waitKey: Option[String] = None,
+    waitTime: Option[Int] = None,
+    signal: Option[Signal] = None) {
 
   def toParams: Seq[(String, Any)] = {
-    val params = db.map(("DB", _)) :: cur.map(("CUR", _)) :: waitKey.map(("WAIT", _)) :: waitTime.map(("WAITTIME", _)) :: Nil
-    params.flatten
+    (db.map(("DB", _)) ::
+      cur.map(("CUR", _)) ::
+      waitKey.map(("WAIT", _)) ::
+      waitTime.map(("WAITTIME", _)) :: Nil).flatten ++
+      signal.map(_.toParams).getOrElse(Nil)
   }
 }
 
 object CommonParams {
   lazy val empty: CommonParams = CommonParams()
+}
+
+case class Signal(value: String, broad: Boolean = false) {
+  def toParams: Seq[(String, Any)] = {
+    val signal = ("SIGNAL", value)
+    if (broad) {
+      Seq(signal, ("SIGNALBROAD", ""))
+    } else {
+      Seq(signal)
+    }
+  }
 }
